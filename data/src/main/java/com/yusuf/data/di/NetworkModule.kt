11b20574b13b52c7b42d3d6da0a7944113e9b6a7
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-25 22:06:37 +0300

    Network Dependencies Added

---
+++ data/src/main/java/com/yusuf/data/di/NetworkModule.kt
@@ -0,0 +1,34 @@
+package com.yusuf.data.di
+
+
+import com.yusuf.data.BuildConfig
+import com.yusuf.data.remote.PharmacyApi
+import com.yusuf.data.util.NetworkConstants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): PharmacyApi {
+        return retrofit.create(PharmacyApi::class.java)
+    }
+}
