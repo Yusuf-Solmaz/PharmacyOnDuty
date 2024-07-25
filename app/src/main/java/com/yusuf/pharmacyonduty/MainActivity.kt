ceca38851af043f36655a8b2fa4d5620b803bf88
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-25 22:05:48 +0300

    Scaffold Update

--- app/src/main/java/com/yusuf/pharmacyonduty/MainActivity.kt
+++ app/src/main/java/com/yusuf/pharmacyonduty/MainActivity.kt
@@ -1,30 +1,28 @@
 package com.yusuf.pharmacyonduty
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.yusuf.feature.home.Greeting
+import com.yusuf.feature.pharmacy.PharmacyOnDuty
 import com.yusuf.pharmacyonduty.ui.theme.PharmacyOnDutyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PharmacyOnDutyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Greeting("Yusuf", modifier = Modifier.padding(innerPadding))
-
-                }
+                Scaffold(modifier = Modifier.fillMaxSize(), content = {
+                    PharmacyOnDuty()
+                })
             }
         }
     }
