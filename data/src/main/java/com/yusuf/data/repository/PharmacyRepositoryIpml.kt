1c967aa96f7f1f38b372037f36c36ddcb4991361
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-25 22:08:24 +0300

    PharmacyRepositoryImpl Completed

---
+++ data/src/main/java/com/yusuf/data/repository/PharmacyRepositoryIpml.kt
@@ -0,0 +1,28 @@
+package com.yusuf.data.repository
+
+import com.yusuf.data.mapper.toPharmacyRoot
+import com.yusuf.data.remote.PharmacyApi
+import com.yusuf.data.util.rootFlow
+import com.yusuf.domain.model.pharmacy.PharmacyRoot
+import com.yusuf.domain.repository.pharmacy.PharmacyRepository
+import com.yusuf.domain.util.RootResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class PharmacyRepositoryImpl @Inject constructor(
+    private val api: PharmacyApi
+) : PharmacyRepository {
+
+     override suspend fun getPharmacyOnDutyByCity(city: String, district: String): Flow<RootResult<PharmacyRoot>> = flow {
+        try {
+            emit(RootResult.Loading)
+            val response =api.getPharmaciesOnDuty(city = city, district = district)
+            emit(RootResult.Success(response.toPharmacyRoot()))
+        }
+        catch (e:Exception){
+            emit(RootResult.Error(e.message ?: "Something went wrong"))
+        }
+    }
+
+}
