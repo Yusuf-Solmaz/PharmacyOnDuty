7e2952b55f4b15f38219ac1ba9802747b0b69145
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-25 22:08:06 +0300

    Method Update

--- domain/src/main/java/com/yusuf/domain/repository/pharmacy/PharmacyRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/pharmacy/PharmacyRepository.kt
@@ -1,11 +1,9 @@
 package com.yusuf.domain.repository.pharmacy
 
 import com.yusuf.domain.model.pharmacy.PharmacyRoot
-import com.yusuf.domain.util.GeneralResult
+import com.yusuf.domain.util.RootResult
 import kotlinx.coroutines.flow.Flow
 
 interface PharmacyRepository {
-
-    suspend fun getPharmacyOnDutyByCity(city: String) : Flow<GeneralResult<PharmacyRoot>>
-
+    suspend fun getPharmacyOnDutyByCity(city: String, district: String): Flow<RootResult<PharmacyRoot>>
 }
