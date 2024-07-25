647cad8c824a72e7c27f86c424233b308de16628
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-25 22:04:29 +0300

    Result Update

--- domain/src/main/java/com/yusuf/domain/util/GeneralResult.kt
+++
@@ -1,7 +0,0 @@
-package com.yusuf.domain.util
-
-sealed class GeneralResult<out R> {
-    data class Success<out R>(val data: R?) : GeneralResult<R>()
-    data class Error(val message:String) : GeneralResult<Nothing>()
-    data object Loading : GeneralResult<Nothing>()
-}
