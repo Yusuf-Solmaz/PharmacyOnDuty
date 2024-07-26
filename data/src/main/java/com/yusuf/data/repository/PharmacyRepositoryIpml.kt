8882f29df5a6d233f580b2972e0769736630fb69
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-07-26 12:04:56 +0300

    flowOn Added

--- data/src/main/java/com/yusuf/data/repository/PharmacyRepositoryIpml.kt
+++ data/src/main/java/com/yusuf/data/repository/PharmacyRepositoryIpml.kt
@@ -2,12 +2,13 @@ package com.yusuf.data.repository
 
 import com.yusuf.data.mapper.toPharmacyRoot
 import com.yusuf.data.remote.PharmacyApi
-import com.yusuf.data.util.rootFlow
 import com.yusuf.domain.model.pharmacy.PharmacyRoot
 import com.yusuf.domain.repository.pharmacy.PharmacyRepository
 import com.yusuf.domain.util.RootResult
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class PharmacyRepositoryImpl @Inject constructor(
@@ -23,6 +24,6 @@ class PharmacyRepositoryImpl @Inject constructor(
         catch (e:Exception){
             emit(RootResult.Error(e.message ?: "Something went wrong"))
         }
-    }
+    }.flowOn(Dispatchers.IO)
 
 }
