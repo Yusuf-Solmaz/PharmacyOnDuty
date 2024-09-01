023cb5663ec6c36e52b9f3f16543c63c93c6c465
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:32:35 +0300

    add dropdown menu (city & district), lazy column (pharmacy on duty list)

--- feature/src/main/java/com/yusuf/feature/pharmacy/PharmacyScreen.kt
+++ feature/src/main/java/com/yusuf/feature/pharmacy/PharmacyScreen.kt
@@ -1,36 +1,186 @@
 package com.yusuf.feature.pharmacy
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.yusuf.domain.model.city.CityRoot
+import com.yusuf.feature.pharmacy.state.PharmacyUIState
+import com.yusuf.feature.pharmacy.viewmodel.PharmacyViewModel
 
 @Composable
-fun PharmacyOnDuty(viewModel: PharmacyViewModel= hiltViewModel()) {
+fun PharmacyOnDuty(viewModel: PharmacyViewModel = hiltViewModel()) {
 
-    val pharmacyOnDutyState by viewModel.rootPharmacyState.collectAsState(PharmacyUIState())
-    if (pharmacyOnDutyState.isLoading) {
-        Text(text = "Loading")
+    val pharmacyUIState by viewModel.pharmacyUIState.collectAsState()
+    val cityState by viewModel.cityState.collectAsState()
+    val districtState by viewModel.districtState.collectAsState()
+
+    val cityExpanded = remember { mutableStateOf(false) }
+    val selectedCity = remember { mutableStateOf("Select a city") }
+    val selectedCitySlug = remember { mutableStateOf("") }
+
+    val districtExpanded = remember { mutableStateOf(false) }
+    val selectedDistrict = remember { mutableStateOf("Select a district") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (cityState.isLoading || districtState.isLoading) {
+            Text(text = "Loading", modifier = Modifier.padding(16.dp))
+        } else {
+            Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
+                Row (Modifier.weight(1f)){
+                    CityDropdown(
+                        cities = cityState.cities,
+                        selectedCity = selectedCity,
+                        selectedCitySlug = selectedCitySlug,
+                        cityExpanded = cityExpanded,
+                        onCitySelected = { citySlug ->
+                            viewModel.fetchDistricts(citySlug)
+                        }
+                    )
+                }
+                Row (Modifier.weight(1f)){
+                    DistrictDropdown(
+                        districts = districtState.cities,
+                        selectedDistrict = selectedDistrict,
+                        districtExpanded = districtExpanded,
+                        onDistrictSelected = { districtSlug ->
+                            viewModel.fetchPharmacyByCity(selectedCitySlug.value, districtSlug)
+                        }
+                    )
+                }
+            }
+
+
+            PharmacyList(pharmacyUIState = pharmacyUIState)
+        }
     }
-    if (pharmacyOnDutyState.error != null) {
-        Text(text = pharmacyOnDutyState.error.toString())
-    }
-    if (pharmacyOnDutyState.rootResponse != null) {
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        )
-        {
-            Text(text = pharmacyOnDutyState.rootResponse!!.data[0].pharmacyName)
+}
+
+@Composable
+fun CityDropdown(
+    cities: CityRoot?,
+    selectedCity: MutableState<String>,
+    selectedCitySlug: MutableState<String>,
+    cityExpanded: MutableState<Boolean>,
+    onCitySelected: (String) -> Unit
+) {
+        Row(
+            modifier = Modifier
+                .wrapContentHeight()
+                .clickable { cityExpanded.value = true }
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = selectedCity.value,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Dropdown Icon"
+            )
         }
 
+        DropdownMenu(
+            modifier = Modifier.size(width = 150.dp, height = 200.dp),
+            expanded = cityExpanded.value,
+            onDismissRequest = { cityExpanded.value = false }
+        ) {
+            cities?.data?.forEach { city ->
+                DropdownMenuItem(
+                    text = { Text(text = city.cities) },
+                    onClick = {
+                        selectedCity.value = city.cities
+                        selectedCitySlug.value = city.slug
+                        cityExpanded.value = false
+                        onCitySelected(city.slug)
+                    }
+                )
+            }
+        }
     }
 
+@Composable
+fun DistrictDropdown(
+    districts: CityRoot?,
+    selectedDistrict: MutableState<String>,
+    districtExpanded: MutableState<Boolean>,
+    onDistrictSelected: (String) -> Unit
+) {
+    if (districts != null) {
+        Row(
+            modifier = Modifier
+                .clickable { districtExpanded.value = true }
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = selectedDistrict.value,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Dropdown Icon"
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier.size(width = 150.dp, height = 200.dp),
+            expanded = districtExpanded.value,
+            onDismissRequest = { districtExpanded.value = false }
+        ) {
+            districts.data.forEach { district ->
+                DropdownMenuItem(
+                    text = { Text(text = district.cities) },
+                    onClick = {
+                        selectedDistrict.value = district.cities
+                        districtExpanded.value = false
+                        onDistrictSelected(district.slug)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+internal fun PharmacyList(pharmacyUIState: PharmacyUIState) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        when {
+            pharmacyUIState.isLoading -> Text(text = "Loading")
+            pharmacyUIState.error != null -> {
+                Text(text = pharmacyUIState.error)
+                Log.e("PharmacyOnDuty", pharmacyUIState.error)
+            }
+            pharmacyUIState.rootResponse != null -> {
+                LazyColumn {
+                    items(pharmacyUIState.rootResponse.data.size) { index ->
+                        Text(text = pharmacyUIState.rootResponse.data[index].pharmacyName)
+                    }
+                }
+            }
+        }
+    }
 }
