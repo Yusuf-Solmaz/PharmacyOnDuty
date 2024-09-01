961225d6c946d2aba9bb90d5817410a8403658b3
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:28:30 +0300

    add getCities

--- data/src/main/java/com/yusuf/data/remote/PharmacyApi.kt
+++ data/src/main/java/com/yusuf/data/remote/PharmacyApi.kt
@@ -1,8 +1,8 @@
 package com.yusuf.data.remote
 
 import com.yusuf.data.BuildConfig
-import com.yusuf.domain.model.pharmacy.PharmacyRootDto
-import retrofit2.Response
+import com.yusuf.data.remote.dto.city.CityRootDto
+import com.yusuf.data.remote.dto.pharmacy.PharmacyRootDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,5 +15,11 @@ interface PharmacyApi {
         @Query("city") city: String,
         @Query("district") district: String
     ): PharmacyRootDto
+
+    @GET("pharmacies-on-duty/cities")
+    suspend fun getCities(
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+        @Query("city") city: String? = null
+    ): CityRootDto
 
 }
