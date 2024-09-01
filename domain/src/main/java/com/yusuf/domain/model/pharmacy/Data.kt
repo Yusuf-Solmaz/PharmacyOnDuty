1d8d2fa50ed34d7a3d823d8e825b2902dbe1b0cc
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:25:53 +0300

    fix data that could be null

--- domain/src/main/java/com/yusuf/domain/model/pharmacy/Data.kt
+++ domain/src/main/java/com/yusuf/domain/model/pharmacy/Data.kt
@@ -1,17 +1,17 @@
 package com.yusuf.domain.model.pharmacy
 
 data class Data(
-    val address: String,
-    val city: String,
-    val directions: String,
-    val district: String,
-    val latitude: Double,
-    val longitude: Double,
-    val pharmacyDutyEnd: String,
-    val pharmacyDutyStart: String,
-    val pharmacyID: Int,
-    val pharmacyName: String,
-    val phone: String,
-    val phone2: String,
-    val town: String
+    val address: String? = "",
+    val city: String? = "",
+    val directions: String? = "",
+    val district: String? = "",
+    val latitude: Double? = 0.0,
+    val longitude: Double? = 0.0,
+    val pharmacyDutyEnd: String? = "",
+    val pharmacyDutyStart: String? = "",
+    val pharmacyID: Int? = 0,
+    val pharmacyName: String = "",
+    val phone: String? = "",
+    val phone2: String? = "",
+    val town: String? = ""
 )
