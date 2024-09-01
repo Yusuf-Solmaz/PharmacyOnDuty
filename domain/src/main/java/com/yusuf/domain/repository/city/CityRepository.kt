18e8880ae5a01c34ae600cb30474afcfab90a75a
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:23:53 +0300

    remove unnecessary suspend

--- domain/src/main/java/com/yusuf/domain/repository/city/CityRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/city/CityRepository.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface CityRepository {
 
-    suspend fun getCities() : Flow<RootResult<CityRoot>>
+    fun getCities(city: String? = null) : Flow<RootResult<CityRoot>>
 
-    suspend fun getDistricts(city: String) : Flow<RootResult<CityRoot>>
 }
