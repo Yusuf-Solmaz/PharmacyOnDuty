53b83c27ab11efd596084a1240877e1c497d1fca
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:24:07 +0300

    add getCities method

---
+++ data/src/main/java/com/yusuf/data/repository/CityRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.yusuf.data.repository
+
+import com.yusuf.data.mapper.toCityRoot
+import com.yusuf.data.remote.PharmacyApi
+import com.yusuf.domain.model.city.CityRoot
+import com.yusuf.domain.repository.city.CityRepository
+import com.yusuf.domain.util.RootResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class CityRepositoryImpl @Inject constructor(
+    private val api: PharmacyApi
+): CityRepository {
+
+    override fun getCities(city: String?): Flow<RootResult<CityRoot>> = flow {
+        emit(RootResult.Loading)
+        try {
+            val response = api.getCities(city = city)
+            emit(RootResult.Success(response.toCityRoot()))
+        } catch (e: Exception) {
+            RootResult.Error(e.message ?: "Something went wrong")
+        }
+    }.flowOn(Dispatchers.IO)
+}
