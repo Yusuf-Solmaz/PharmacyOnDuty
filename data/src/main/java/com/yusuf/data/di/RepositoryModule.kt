6b001c92b7bbbd7bdd40dacd90a586cd1bcf6fa1
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-01 21:30:48 +0300

    add city repository to hilt module

--- data/src/main/java/com/yusuf/data/di/RepositoryModule.kt
+++ data/src/main/java/com/yusuf/data/di/RepositoryModule.kt
@@ -1,9 +1,10 @@
 package com.yusuf.data.di
 
 import com.yusuf.data.remote.PharmacyApi
+import com.yusuf.data.repository.CityRepositoryImpl
 import com.yusuf.data.repository.PharmacyRepositoryImpl
+import com.yusuf.domain.repository.city.CityRepository
 import com.yusuf.domain.repository.pharmacy.PharmacyRepository
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,6 +19,12 @@ object RepositoryModule {
     @Singleton
     fun providePharmacyRepository(api: PharmacyApi): PharmacyRepository {
         return PharmacyRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCityRepository(api: PharmacyApi): CityRepository {
+        return CityRepositoryImpl(api)
     }
 
 
