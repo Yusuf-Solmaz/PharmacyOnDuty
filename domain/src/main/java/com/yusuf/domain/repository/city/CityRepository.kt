8f13c5f77e5563dd3c7274ab43453eb64d4783d1
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-09-08 19:35:06 +0300

    change repository fun return type

--- domain/src/main/java/com/yusuf/domain/repository/city/CityRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/city/CityRepository.kt
@@ -1,11 +1,10 @@
 package com.yusuf.domain.repository.city
 
-import com.yusuf.domain.model.city.CityRoot
+import com.yusuf.domain.model.base.RootData
+import com.yusuf.domain.model.city.CityData
 import com.yusuf.domain.util.RootResult
 import kotlinx.coroutines.flow.Flow
 
 interface CityRepository {
-
-    fun getCities(city: String? = null) : Flow<RootResult<CityRoot>>
-
+    fun getCities(city: String? = null): Flow<RootResult<RootData<CityData>>>
 }
--- domain/src/main/java/com/yusuf/domain/repository/pharmacy/PharmacyRepository.kt
+++ domain/src/main/java/com/yusuf/domain/repository/pharmacy/PharmacyRepository.kt
@@ -1,9 +1,10 @@
 package com.yusuf.domain.repository.pharmacy
 
-import com.yusuf.domain.model.pharmacy.PharmacyRoot
+import com.yusuf.domain.model.base.RootData
+import com.yusuf.domain.model.pharmacy.PharmacyData
 import com.yusuf.domain.util.RootResult
 import kotlinx.coroutines.flow.Flow
 
 interface PharmacyRepository {
-    fun getPharmacyOnDutyByCity(city: String, district: String): Flow<RootResult<PharmacyRoot>>
+    fun getPharmacyOnDutyByCity(city: String, district: String): Flow<RootResult<RootData<PharmacyData>>>
 }
